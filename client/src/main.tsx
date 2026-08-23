9e1a171b4326207974750e35f7e12f4db218b29f
Doug <64031739+MishaGromovPrlmn@users.noreply.github.com>
2026-08-23 09:42:53 -0400

    Add main; enable providers

---
+++ client/src/main.tsx
@@ -0,0 +1,29 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter } from 'react-router-dom'
+import { DataProvider } from '@data-client/react'
+
+
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
+
+const queryClient = new QueryClient()
+
+createRoot(document.getElementById('root')!).render(
+  <QueryClientProvider client={queryClient}>
+
+    <MantineProvider>
+    <DataProvider> 
+    <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </DataProvider>
+    </MantineProvider>
+  </QueryClientProvider>,
+)
+
+
+
