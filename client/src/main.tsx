0e7e9175156e286bcd7c285c201418924fcfb684
Doug <64031739+MishaGromovPrlmn@users.noreply.github.com>
2026-08-26 20:04:15 -0400

    update import after refactoring

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './Home.tsx'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter } from 'react-router-dom'
